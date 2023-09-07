6fa10977e63a064554bed8725455cf436c3aeab1
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2023-09-07 19:06:37 +0300

    Added SignUpUseCase into the project.

---
+++ app/src/main/java/com/huseyinozkoc/bitcointicker/domain/usecase/SignUpUseCase.kt
@@ -0,0 +1,12 @@
+package com.huseyinozkoc.bitcointicker.domain.usecase
+
+import com.huseyinozkoc.bitcointicker.domain.FirebaseRepository
+import javax.inject.Inject
+
+class SignUpUseCase @Inject constructor(private val firebaseRepository: FirebaseRepository) {
+
+    operator fun invoke(email: String, password: String) {
+        firebaseRepository.signUpWithEmailAndPassword(email, password)
+    }
+
+}
