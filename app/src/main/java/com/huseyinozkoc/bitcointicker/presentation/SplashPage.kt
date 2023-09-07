e0b92788f156b0f90b829835730d7083fe5117a5
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2023-09-07 15:08:22 +0300

    Added ViewBinding into the project and created ViewBindingDelegate

--- app/src/main/java/com/huseyinozkoc/bitcointicker/presentation/SplashPage.kt
+++ app/src/main/java/com/huseyinozkoc/bitcointicker/presentation/SplashPage.kt
@@ -1,14 +1,23 @@
 package com.huseyinozkoc.bitcointicker.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.huseyinozkoc.bitcointicker.R
+import com.huseyinozkoc.bitcointicker.common.viewBinding
+import com.huseyinozkoc.bitcointicker.databinding.FragmentSplashPageBinding
+import kotlinx.coroutines.Dispatchers
 
 
 class SplashPage : Fragment() {
+
+
+    private val binding by viewBinding(FragmentSplashPageBinding::bind)
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +25,15 @@ class SplashPage : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.appLogo.setOnClickListener {
+
+        }
+
     }
 
 }
