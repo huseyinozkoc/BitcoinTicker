b9f0561d5807956233f5f634a3a86189d46ed51b
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2023-09-08 19:51:54 +0300

    Added Analytics, Crashlytics, App Performance Manager into the project.

--- app/src/main/java/com/huseyinozkoc/bitcointicker/di/FirebaseModule.kt
+++ app/src/main/java/com/huseyinozkoc/bitcointicker/di/FirebaseModule.kt
@@ -1,7 +1,11 @@
 package com.huseyinozkoc.bitcointicker.di
 
+import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.perf.FirebasePerformance
 import com.huseyinozkoc.bitcointicker.data.repository.FirebaseRepositoryImpl
 import com.huseyinozkoc.bitcointicker.domain.repository.FirebaseRepository
 import dagger.Module
@@ -23,6 +27,21 @@ object FirebaseModule {
     @Singleton
     @Provides
     fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAnalytics(application: Application): FirebaseAnalytics {
+        return FirebaseAnalytics.getInstance(application)
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirebasePerformance(): FirebasePerformance = FirebasePerformance.getInstance()
+
+    @Singleton
+    @Provides
+    fun provideFirebaseCrashlytics(): FirebaseCrashlytics = FirebaseCrashlytics.getInstance()
 
     @Provides
     @Singleton
--- app/src/main/java/com/huseyinozkoc/bitcointicker/presentation/SplashPage/SplashPage.kt
+++ app/src/main/java/com/huseyinozkoc/bitcointicker/presentation/SplashPage/SplashPage.kt
@@ -6,25 +6,40 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.huseyinozkoc.bitcointicker.R
 import com.huseyinozkoc.bitcointicker.common.viewBinding
 import com.huseyinozkoc.bitcointicker.databinding.FragmentSplashPageBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class SplashPage : Fragment() {
 
 
     private val binding by viewBinding(FragmentSplashPageBinding::bind)
+
+    @Inject
+     lateinit var analytics: FirebaseAnalytics
+
+    @Inject
+    lateinit var crashlytics: FirebaseCrashlytics
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        analytics.setAnalyticsCollectionEnabled(true)
+
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_page, container, false)
     }
@@ -32,10 +47,13 @@ class SplashPage : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
         CoroutineScope(Dispatchers.Main).launch {
 
             delay(5000)
             findNavController().navigate(R.id.action_splashPage_to_signInPage)
+            //crashlytics.recordException(throw RuntimeException("Test Crash"))
         }
 
 
